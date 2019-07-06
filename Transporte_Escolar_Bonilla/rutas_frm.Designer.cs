c39eadbe655be28d009019ede6c31b4ce9dc47d7
Eric Rodriguez <honduras.eric@gmail.com>
2019-07-06 14:58:56 -0600

    Cambios Esteticos

--- Transporte_Escolar_Bonilla/rutas_frm.Designer.cs
+++ Transporte_Escolar_Bonilla/rutas_frm.Designer.cs
@@ -120,7 +120,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Window;
-            this.label1.Location = new System.Drawing.Point(77, 509);
+            this.label1.Location = new System.Drawing.Point(31, 509);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(954, 80);
             this.label1.TabIndex = 11;
